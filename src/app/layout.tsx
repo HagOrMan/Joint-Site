d6bdfefd37969328fbb026e3adc477cd879aad3b
HagOrMan <khagypi3@gmail.com>
2023-08-21 23:07:02 -0400

    added second navbar from template for reference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Navbar } from './navbar'
+// import { Navbar } from './navbar2'
 
 const inter = Inter({ subsets: ['latin'] })
 
