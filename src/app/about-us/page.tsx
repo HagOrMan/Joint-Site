fbf983b644d2a79c609b9cfedfabdd42734d6756
HagOrMan <khagypi3@gmail.com>
2023-09-14 10:25:06 -0400

    #5 Added more info about us and our projects

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -21,16 +21,25 @@ export default function Page() {
       </div>
 
       {/* Explain our motivation for creating the website */}
-      <div className='Motivation '>
+      <div className='Motivation text-left p-6'>
         <h1 className='font-bold text-justify'>Why a Joint Website?</h1>
-        <p>Making boring old personal websites is so clich&#233;, don&apos;t you think?</p>
+        <p>
+          Making boring old personal websites is so clich&#233;, don&apos;t you think? It&apos;s all &quot;About Me&quot;, but what &quot;About Us&quot;?
+          <br/>
+          That&apos;s why we decided to make a website together, two passionate software engineers showing off our projects and collaboration skills in a group portfolio!
+        </p>
       </div>
 
-      {/* More on us? */}
-      <div className='text-left'>
-        <h1 className='font-bold'>Our Experience</h1>
-        <p>We are both avid programmers and love completing projects together. Our most notable is our DeltaHacks 10 
-          winning app <a href='/medisafe' className='underline'>MediSafe</a>!</p>
+      {/* Our Experience, basically more about us */}
+      <div className='Our-Experience text-left p-6'>
+        <h1 className='font-bold text-justify'>Our Experience</h1>
+        <p>
+          We are both avid programmers and love completing projects together. Our most notable is our DeltaHacks 10 
+          winning app <a href='/medisafe' className='underline'>MediSafe</a>!
+          <br/>
+          You can also check out our resumes using the dropdown below! 
+        </p>
+        {/* TODO: Create dropdown component linking to pages with our resumes */}
       </div>
     </main>
   )
